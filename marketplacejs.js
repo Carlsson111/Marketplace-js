426e4bef571a58d9452b6dddee77b229441c3fcb
Carlsson111 <Linusjaa1@gmail.com>
2025-03-06 09:51:42 +0100

    Add marketplace HTML and JS files

---
+++ marketplacejs.js
@@ -0,0 +1,38 @@
+const adVertimentsData = [
+    {
+        title: 'Ad 1',
+        description: 'This is the first advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'contact1@example.com'
+    },
+    {            
+        title: 'Ad 2',
+        description: 'This is the second advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'test@test.nu'
+    },
+    {
+        title: 'Ad 3',
+        description: 'This is the third advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'figgeferrum@example.com'
+    },
+    {
+        title: 'Ad 4',
+        description: 'This is the first advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'Anders@test.nu'
+    },
+    {
+        title: 'Ad 5',
+        description: 'This is the first advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'randomemail@test.com'
+    },
+    {
+        title: 'Ad 6',
+        description: 'This is the first advertisement.',
+        image: 'https://via.placeholder.com/150',
+        contact: 'contact3@example.com'
+    }
+];
